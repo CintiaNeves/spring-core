921f15311aeb37463f0bd2391d77266eed1119ec
Cintia Pedroso <ndd_mpedroso@uolinc.com>
2021-01-23 19:35:07 -0300

    Implements an example of dependency injection using the ApplicationContext and gets a component instance by bean name.

---
+++ src/main/java/br/com/pdi/springcore/HelloWorld.java
@@ -0,0 +1,10 @@
+package br.com.pdi.springcore;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+    public void sayHello(){
+        System.out.println("Hello World!!!");
+    }
+}
--- src/main/java/br/com/pdi/springcore/SpringCoreApplication.java
+++ src/main/java/br/com/pdi/springcore/SpringCoreApplication.java
@@ -2,12 +2,16 @@ package br.com.pdi.springcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringCoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCoreApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(SpringCoreApplication.class, args);
+		HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
+		helloWorld.sayHello();
 	}
 
 }
