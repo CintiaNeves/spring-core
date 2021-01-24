0b3fcababbfb22398b0a19ac76955530aff5ee83
Cintia Pedroso <ndd_mpedroso@uolinc.com>
2021-01-23 22:10:42 -0300

    Using bean factory.

--- src/main/java/br/com/pdi/springcore/config/HelloWorldConfig.java
+++ src/main/java/br/com/pdi/springcore/config/HelloWorldConfig.java
@@ -1,9 +1,7 @@
 package br.com.pdi.springcore.config;
 
 import br.com.pdi.springcore.service.HelloWorldService;
-import br.com.pdi.springcore.service.impl.HelloWorldServiceBrazilianImpl;
-import br.com.pdi.springcore.service.impl.HelloWorldServiceEnglishImpl;
-import br.com.pdi.springcore.service.impl.HelloWorldServiceEspanishImpl;
+import br.com.pdi.springcore.service.impl.HelloWorldServiceFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,20 +10,25 @@ import org.springframework.context.annotation.Profile;
 public class HelloWorldConfig {
 
     @Bean
-    @Profile("english")
-    public HelloWorldService helloWorldServiceEnglish(){
-        return new HelloWorldServiceEnglishImpl();
+    public HelloWorldServiceFactory helloWorldServiceFactory(){
+        return new HelloWorldServiceFactory();
+    }
+
+    @Bean
+    @Profile({"english", "default"})
+    public HelloWorldService helloWorldServiceEnglish(HelloWorldServiceFactory factory){
+        return factory.createHelloWorldService("en");
     }
 
     @Bean
     @Profile("espanish")
-    public HelloWorldService helloWorldServiceEspanishImpl(){
-        return new HelloWorldServiceEspanishImpl();
+    public HelloWorldService helloWorldServiceEspanishImpl(HelloWorldServiceFactory factory){
+        return factory.createHelloWorldService("es");
     }
 
     @Bean
     @Profile("brazilian")
-    public HelloWorldService helloWorldServiceBrazilianImpl(){
-        return new HelloWorldServiceBrazilianImpl();
+    public HelloWorldService helloWorldServiceBrazilianImpl(HelloWorldServiceFactory factory){
+        return factory.createHelloWorldService("br");
     }
 }
---
+++ src/main/java/br/com/pdi/springcore/service/impl/HelloWorldServiceFactory.java
@@ -0,0 +1,24 @@
+package br.com.pdi.springcore.service.impl;
+
+import br.com.pdi.springcore.service.HelloWorldService;
+
+public class HelloWorldServiceFactory {
+
+    public HelloWorldService createHelloWorldService(String language){
+        HelloWorldService service = null;
+
+        switch (language){
+            case "en":
+                service = new HelloWorldServiceEnglishImpl();
+                break;
+            case "es":
+                service = new HelloWorldServiceEspanishImpl();
+                break;
+            case "br":
+                service = new HelloWorldServiceBrazilianImpl();
+                break;
+            default: new HelloWorldServiceEnglishImpl();
+        }
+        return service;
+    }
+}
