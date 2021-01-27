5e6388bf6bca8eef2aabefeaca1779fd6f361137
Cintia Pedroso <ndd_mpedroso@uolinc.com>
2021-01-27 20:45:13 -0300

    Display a Product

--- src/main/java/br/com/pdi/springcore/controller/ProductController.java
+++ src/main/java/br/com/pdi/springcore/controller/ProductController.java
@@ -4,6 +4,7 @@ import br.com.pdi.springcore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,7 +20,12 @@ public class ProductController {
     @RequestMapping("/products")
     public String listProducts(Model model){
         model.addAttribute("products", productService.listAllProducts());
-
         return "products";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProduct(@PathVariable Long id, Model model){
+        model.addAttribute("product", productService.getProductById(id));
+        return "product";
     }
 }
